ba9a35329b22d5a408df610b0aeab03ebad1d586
TitoshNayak <nayaktitosh@gmail.com>
2021-08-01 05:55:54 +0530

    commit 1

--- Basic.js
+++ Basic.js
@@ -1,8 +1,27 @@
 /*var a=10;
 console.log(a);
-*/
+
 var i=1;
 for(var i=1;i<=10;i++)
 {
     console.log(i);
 }
+*/
+                              //Javascript Variables
+
+//let num =6;
+//console.log(num);
+
+//let num;
+//num=5;
+//console.log(num);
+
+//let num=6;
+//let numb=8;
+//let numc=9;
+//let numd=num+numb+numc;
+//console.log(numd);
+
+
+let num1=4, num2=5,num3=7;
+console.log(num3);
