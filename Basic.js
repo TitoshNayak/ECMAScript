01d47598967537b38835be8cb0920070fce88f86
TitoshNayak <nayaktitosh@gmail.com>
2021-08-01 06:26:49 +0530

    commit 2

--- Basic.js
+++ Basic.js
@@ -23,5 +23,43 @@ for(var i=1;i<=10;i++)
 //console.log(numd);
 
 
-let num1=4, num2=5,num3=7;
-console.log(num3);
+//let num1=4, num2=5,num3=7;
+//console.log(num3);
+
+                //Rules of naming the javascript variables
+/*
+             1.The variable name must start with letter(Alphsbets),underscore or doller Sign.
+             2.The variable  does not start with Number.
+             3.Javascript is case sensitive.
+             4. Keywords cannot be used as a variabel name.
+*/
+
+                 //  Javascript Constant Variabel
+
+//const x=5;
+//console.log(x);
+
+//const x=6;
+//x=10;
+//console.log(x);
+
+//const x;   // initializer in const is missing 
+//x=8;
+//console.log(x);
+
+                // Sentence to execute
+
+//console.log("I like the coding part to be printed.");
+
+               //Program to print variable value
+/*
+    const greet = 'Hello';
+    const Name = 'Jack';
+    console.log(greet + '  '+ Name);
+*/
+
+const Name= 'Nayak';
+const clas= 'Final Year';
+const Roll= 100;
+
+console.log(Name +''+clas+''+Roll);
