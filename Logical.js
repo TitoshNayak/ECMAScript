81ee1d5f9ec075233eb61ea9e4e63d27f5700714
TitoshNayak <nayaktitosh@gmail.com>
2021-08-05 00:03:33 +0530

    Comparison and logical operators

---
+++ Logical.js
@@ -0,0 +1,22 @@
+      // link: https://www.programiz.com/javascript/comparison-logical
+const a =5,  b = 2, c = 'hello';
+
+//console.log(a == 5);
+//console.log(b ==5);
+//console.log(c =='Hello');
+
+
+//console.log(a != 5);
+//console.log(b !=5);             //not equal to
+//console.log(c !='Hello');
+
+
+
+//console.log(a === 5);
+//console.log(b ===5);             //equal to
+//console.log(c ==='Hello');
+
+
+console.log(a !== 5);
+console.log(b !==5);             // strict not equal to
+console.log(c !=='Hello');
