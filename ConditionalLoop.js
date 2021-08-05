f36baec3c26ec94b2f4766ccd6e20c23b9c89795
TitoshNayak <nayaktitosh@gmail.com>
2021-08-05 09:06:29 +0530

    conditional loops

---
+++ ConditionalLoop.js
@@ -0,0 +1,67 @@
+// link :https://www.programiz.com/javascript/if-else
+
+//In JavaScript, there are three forms of the if...else statement.
+/*
+1.if statement
+2.if...else statement
+3.if...else if...else statement
+*/
+
+
+//1
+/*
+let a=8;
+if (a>5)
+{
+    console.log( "Print the number="+a);
+}
+
+//check if the number is positive or not
+
+const number = prompt("Enter the number:");
+
+if (number > 0)
+{
+    console.log("The number is positive:" +number);
+}
+
+
+           //JavaScript if...else statement
+
+           let age=18;
+
+           if (age > 18)
+           {console.log("He is adult of:" +age)
+
+           }
+           else{
+               console.log("He is a child less than :" +age)
+           }
+
+        
+
+           let num=2;
+
+           if(num < 0){
+               console.log(+num);
+           }else{
+              console.log("the number is negative :"+num);
+            }
+
+            */
+
+
+            let num = prompt("Enter a number");
+
+            if(num > 0)
+            {
+                      console.log("The number is positive");
+            }
+
+            else if(num = 0)
+            {
+                console.log("the number is equal");
+            }
+            else {
+                console.log("the number is negative");
+            }
